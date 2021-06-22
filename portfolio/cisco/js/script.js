812ca31619147eaee88d7434c47e33cad6036329
andarpratama <master.amarta@gmail.com>
2021-06-22 08:43:59 +0700

    first commit

---
+++ portfolio/cisco/js/script.js
@@ -0,0 +1,31 @@
+$('.carousel').carousel({
+    interval: 4000,
+    pause: false, //ketika di kursor di atas, bisa hover atau false
+    wrap: false, //looping kembali ke awal
+    touch: true
+})
+
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $(".fixed-top");
+        let link = $(".page-scroll");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+});
+
+
+$('.page-scroll').on('click', function (e) {
+
+    var tujuan = $(this).attr('href');
+
+    var elemenTujuan = $(tujuan);
+
+    // console.log(elemenTujuan.offset().top);
+    // console.log($('html, body').scrollTop())
+
+    $('html , body').animate({
+        scrollTop: elemenTujuan.offset().top - 72
+    }, 1000, 'swing');
+
+    e.preventDefault();
+});
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+       fontFamily: {
+       'montserrat': ['ui-sans-serif', 'system-ui']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
